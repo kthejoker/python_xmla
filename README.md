bd5e07fc08db26d6c319fc4f3baf1b972b7f0300
kthejoker <kthejoker>
2025-07-31 18:08:24 +0000

    clean up

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 Connecting to XMLA endpoint from Python.
 
 Steps
-test
+
+1. I have provided the 5 Analysis Services DLLs below from a SQL Server 2019 installation. You may need to update these with DLLs for your particular XMLA endpoint's compatibility. 
+    * Microsoft.AnalysisServices.AdomdClient.dll
+    * Microsoft.AnalysisServices.Runtime.Core.dll
+    * Microsoft.AnalysisServices.Runtime.Windows.dll
+    * Microsoft.Identity.Client.dll
+    * Microsoft.IdentityModel.Abstractions.dll
+2. Upload these DLLs to a directory in a Unity Catalog volume.
+3. Configure the notebook in the repository with your appropriate connection string for your XMLA endpoint, and the path of your UC volume directory from step 2.
+4. **If Interactive**:
+    * Open the web terminal on your cluster. Run the following command: ```sudo apt-get update && apt-get install -y dotnet-runtime``` and finish installation.
+5. **If Automated**:
+    * Add the command above to the init script of your cluster.
+
+Post any issues here.
--- Read Power BI XMLA.py
+++ Read Power BI XMLA.py
@@ -25,34 +25,28 @@ dbutils.library.restartPython()
 # COMMAND ----------
 
 from sys import path
-path.append("/Volumes/powerbi_demos/nytaxi/powerbi/dlls")
+path.append("/Volumes/my_volume/xmla_dlls")
 from pythonnet import load
 load("coreclr")
 import clr
 from pyadomd import Pyadomd
 from pyspark.sql.types import StructType, StructField, StringType
-#print(token)
-conn= (
-      
+conn= (   
     'Provider=MSOLAP;'
-    'Data Source=powerbi://api.powerbi.com/v1.0/myorg/40f113e7-13d8-47c1-b3e0-76fc609c5948;'
-    'Initial Catalog=PBI_UCMetrics;'  # Replace with actual dataset name
+    f'Data Source=powerbi://api.powerbi.com/v1.0/myorg/{powerbi_group_id};'
+    f'Initial Catalog={powerbi_dataset};'  # Replace with actual dataset name
     f'User ID={user};Password={password};'
     'Persist Security Info=True;'
     'Impersonation Level=Impersonate;'
-
-
         )
-query = "Select * from $SYSTEM.TMSCHEMA_MEASURES/Volumes/powerbi_demos/nytaxi/powerbi/dlls/"
+query = "Select * from $SYSTEM.TMSCHEMA_MEASURES"
 with Pyadomd(conn) as conn:
     with conn.cursor().execute(query) as cur:
         names = [ x[0] for x in cur.description]
         schema = StructType([StructField(name, StringType(), True) for name in names])
-        r =cur.fetchall()
-        #print(r)
-        df = spark.createDataFrame(r, schema)
+        result =cur.fetchall()
+        df = spark.createDataFrame(result, schema)
 display(df)
 # save df(s) to table(s)
 # create Delta Share of tables
-
-
+# knock your socks off
